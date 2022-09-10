a74aa42180e88bc09447e246cbdb2a7b4ea482e6
NateLapak <Nate.lapak12@gmail.com>
2022-09-10 08:16:13 -0600

    Removed api folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Route, Routes, Navigate, useNavigate} from "react-router-dom";
+import {Route, Routes, Navigate} from "react-router-dom";
 
 import NavbarMain from "./Components/Navbar/Navbar";
 import Homepage from "./Pages/Homepage"
