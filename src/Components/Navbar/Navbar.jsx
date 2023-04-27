3a1784d985f8eb4733a9d4b663b8eca9399a23a1
NateLapak <Nate.lapak12@gmail.com>
2023-04-26 22:37:40 -0600

    Added hover styling for navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,30 +11,30 @@ const NavbarMain = () => {
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
 
-                {/* Full name and Brand */}
-                <NavLink to="/homepage" style={{textDecoration: "none"}}>
-                    <Navbar.Brand className="navbarHeader">Nathan Lapak</Navbar.Brand>
-                </NavLink>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-
-                {/* Sub-sections of websites */}
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="ms-auto">
-                    
-                    <NavLink to="/about" style={{textDecoration: "none"}}>
-                        <Nav.Link href="#about" className="navbarLink">About</Nav.Link>
+                    {/* Full name and Brand */}
+                    <NavLink to="/homepage" style={{textDecoration: "none"}}>
+                        <Navbar.Brand className="navbarHeader">Nathan Lapak</Navbar.Brand>
                     </NavLink>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        
 
-                    <NavLink to="/blog" style={{textDecoration: "none"}}>
-                        <Nav.Link href="/blog" className="navbarLink">Blog</Nav.Link>
-                    </NavLink>
-                    
-                    <NavLink to="/contact" style={{textDecoration: "none"}}>
-                        <Nav.Link href="#Contact" className="navbarLink">Contact</Nav.Link>
-                    </NavLink>
+                    {/* Sub-sections of websites */}
+                    <Navbar.Collapse id="responsive-navbar-nav">
+    
+                        <Nav className="ms-auto">
+                            <NavLink to="/about" style={{textDecoration: "none"}}>
+                                <Nav.Link href="#about" className="navbarLink">About</Nav.Link>
+                            </NavLink>
 
-                    </Nav>
-                </Navbar.Collapse>
+                            <NavLink to="/blog" style={{textDecoration: "none"}}>
+                                <Nav.Link href="#blog" className="navbarLink">Blog</Nav.Link>
+                            </NavLink>
+                            
+                            <NavLink to="/contact" style={{textDecoration: "none"}}>
+                                <Nav.Link href="#contact" className="navbarLink">Contact</Nav.Link>
+                            </NavLink>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </div>
